8eec9f328d809cdce885455c8e1cadc26609dd3f
kenanforto <132957986+kenanforto@users.noreply.github.com>
2025-04-25 19:11:10 +0200

    Revert "Jwt"

--- Hotel/src/main/java/com/hotel/Hotel/controllers/UserController.java
+++ Hotel/src/main/java/com/hotel/Hotel/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.hotel.Hotel.controllers;
 
-
 import com.hotel.Hotel.models.Role;
 import com.hotel.Hotel.models.RoomStatus;
 import com.hotel.Hotel.models.User;
@@ -8,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -59,63 +60,5 @@ public class UserController {
             log.error("Error fetching users", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of());
         }
-    }
-    @GetMapping(path="{username}")
-    public ResponseEntity<User> getByName(@PathVariable String username) {
-        try {
-            var resultSet = jdbcConnection.createStatement().executeQuery("SELECT * FROM NBP.NBP_USER WHERE username='" + username + "'");
-            System.out.println(resultSet);
-            var user = new User();
-            while (resultSet.next()) {
-                user.setId(resultSet.getInt("ID"));
-                user.setFirstName(resultSet.getString("FIRST_NAME"));
-                user.setLastName(resultSet.getString("LAST_NAME"));
-                user.setEmail(resultSet.getString("EMAIL"));
-                user.setUsername(resultSet.getString("USERNAME"));
-                user.setPassword(resultSet.getString("PASSWORD"));
-                user.setRoleId(resultSet.getInt("ROLE_ID"));
-                user.setAddressId(resultSet.getInt("ADDRESS_ID"));
-            }
-            System.out.println(user);
-
-            return ResponseEntity.ok(user);
-        } catch (SQLException e) {
-            log.error("Error fetching users", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new User());
-        }
-    }
-
-    @PostMapping()
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
-        User saveUser;
-        try {
-            saveUser = new User(
-                    user.getFirstName(),
-                    user.getLastName(),
-                    user.getEmail(),
-                    user.getPassword(),
-                    user.getUsername(),
-                    user.getPhoneNumber(),
-                    user.getBirthDate(),
-                    user.getAddressId(),
-                    user.getRoleId()
-            );
-            System.out.println(saveUser);
-            var prepareStatement = jdbcConnection.prepareStatement("INSERT INTO NBP.NBP_USER (ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ADDRESS_ID, ROLE_ID) VALUES(nbp.nbp_user_id_seq.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            prepareStatement.setString(1,saveUser.getFirstName());
-            prepareStatement.setString(2,saveUser.getLastName());
-            prepareStatement.setString(3,saveUser.getEmail());
-            prepareStatement.setString(4,saveUser.getPassword());
-            prepareStatement.setString(5,saveUser.getUsername());
-            prepareStatement.setString(6,saveUser.getPhoneNumber());
-            prepareStatement.setDate(7,saveUser.getBirthDate());
-            prepareStatement.setInt(8,saveUser.getAddressId());
-            prepareStatement.setInt(9,saveUser.getRoleId());
-            var resultSet=prepareStatement.executeQuery();
-        } catch (SQLException e) {
-            log.error("Error fetching users", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new User());
-        }
-        return ResponseEntity.ok(saveUser);
     }
 }
--- Hotel/src/main/java/com/hotel/Hotel/models/Role.java
+++ Hotel/src/main/java/com/hotel/Hotel/models/Role.java
@@ -15,19 +15,4 @@ public class Role {
     private Integer id;
     private String name;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
